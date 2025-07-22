fef3b35c97fedb05489ee3bb5d54e361bf4f02ff
Ressane Messioughi <ressane.messioughi@laplateforme.io>
2025-07-22 16:50:09 +0200

    Runtrack2 Jour3

---
+++ jour03/index.php
@@ -0,0 +1,30 @@
+<!-- 
+Créez un tableau contenant les nombres 200, 204, 173, 98, 171, 404, 459.
+Parcourez ce tableau et affichez pour chaque nombre “X est paire<br />” ou “X est
+impaire<br />”, sur votre page index.php. X prenant tour à tour chacune des valeurs
+comprises dans ce tableau.
+Ex. : 200 est paire
+204 est paire
+173 est impaire
+98 est paire
+171 est impaire
+404 est paire
+459 est impaire -->
+
+
+<?php
+
+$tableau = array(200,204,173,98,171,404,459);
+
+foreach ($tableau as $nombre){
+    if ($nombre % 2 == 0) {
+        echo "$nombre est paire <br/>";
+
+    }
+    else {
+        echo "$nombre est impair <br/>";
+    }
+}
+
+
+?>
